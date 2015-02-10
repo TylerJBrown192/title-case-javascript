37eaf8d06df7b0efa2e9e2a216d236e3879f32ab
Joey Sandine and Tyler Brown <elementalplug@gmail.com>
2015-02-10 13:54:26 -0800

    Create a loop for multiple capitalized words

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 var titleCase = function(user_string) {
-  return user_string.charAt(0).toUpperCase() + user_string.slice(1);
+  var fields = user_string.toLowerCase().split(" ");
+  var results = [];
+
+  fields.forEach(function(user_array) {
+    var new_array = user_array.charAt(0).toUpperCase() + user_array.slice(1);
+    results.push(new_array);
+  });
+  return results.join(" ")
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("titleCase", function() {
   it("capitalizes the first letter of a word", function() {
     expect(titleCase("cat")).to.equal("Cat");
   });
+
+  it("capitalizes the first letter of two words", function() {
+    expect(titleCase("the dog")).to.equal("The Dog");
+  });
 });
