e0222fc7b3e8cc38afef703ff7b8a3997da24353
Joey Sandine and Tyler Brown <elementalplug@gmail.com>
2015-02-10 15:59:47 -0800

    Fix the broken code and properly exempt of from capitalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 var titleCase = function(user_string) {
   var initialResults = user_string.toLowerCase().split(" ");
-  var results = [];
-  var moreResults = [];
+  var results = Array();
+  var moreResults = Array();
   var exceptions = "Of";
 
   initialResults.forEach(function(singled_word) {
     var new_capitalized_words = singled_word.charAt(0).toUpperCase() + singled_word.slice(1);
     results.push(new_capitalized_words);
-    results.forEach(function(small_word) {
-      if (small_word === exceptions) {
-        var new_small_word = small_word.toLowerCase();
-        moreResults.push(new_small_word);
-    } else {
-        moreResults.push(small_word)
-      };
     });
+
+  results.forEach(function(small_word) {
+    if (small_word === exceptions) {
+      var new_small_word = small_word.toLowerCase();
+      moreResults.push(new_small_word);
+    } else {
+      moreResults.push(small_word);
+    };
   });
+
   return moreResults.join(" ");
 };
